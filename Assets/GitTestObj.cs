7eaa26c1c61dc362a40685f91ee42e9eeb531963
PuzzleNoHakobiyaOnline <leev.str@gmail.com>
2021-10-17 23:46:40 +0900

    テストです！
    文字出力しました

---
+++ Assets/GitTestObj.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTestObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ê≥èÌÇ…òAågÇ≈Ç´ÇΩÇÊ!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
